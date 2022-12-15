5f0b144c73d27ae5aa7207819ba5a3d78e7fbba3
clifford <cliffmurimi@gmail.com>
2022-12-15 21:50:33 +0300

    more

--- to_do_list/views.py
+++ to_do_list/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.models import User,auth
 from .models import *
-from django.contrib.auth.decorators import login_required
+#from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-@login_required(login_url='login')
+#@login_required(login_url='login')
 def dashboard(request):
     if request.method=="POST":
         if request.user is not None:
