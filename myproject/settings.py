79db1acf7bcffcd25ec3104f4589f4cfabb7f62b
clifford <cliffmurimi@gmail.com>
2022-12-16 07:48:14 +0300

    more

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-22+*=u_()l%17^igfe1_*!@!7w@ss$i9qiv##2x+6z1#_&^#i2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','web-production-7f22.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
